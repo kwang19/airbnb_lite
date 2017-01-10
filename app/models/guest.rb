a3ee6025dce84b62da9000d5c8b7c223ea1a1017
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:43:10 +0000

    Direct association between guests and messages

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,6 +1,10 @@
 class Guest < ApplicationRecord
   # Direct associations
 
+  has_many   :messages,
+             :foreign_key => "guests_id",
+             :dependent => :destroy
+
   has_many   :bookings,
              :foreign_key => "guests_id",
              :dependent => :destroy
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ApplicationRecord
   # Direct associations
 
+  belongs_to :guests
+
   # Indirect associations
 
   # Validations
