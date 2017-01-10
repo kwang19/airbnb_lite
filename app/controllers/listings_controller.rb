349c6280a2a13ab630731ee30bcc8758dbd771a8
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:44:00 +0000

    Only listing user can modify listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,14 @@
 class ListingsController < ApplicationController
+  before_action :current_user_must_be_listing_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_listing_host
+    listing = Listing.find(params[:id])
+
+    unless current_user == listing.host
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @listings = Listing.all
 
