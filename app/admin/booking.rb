28ac0d216352188af54521acd992bd40e6499cfd
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:43:33 +0000

    Whitelist columns in Booking for ActiveAdmin

--- app/admin/booking.rb
+++ app/admin/booking.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Booking do
 
+ permit_params :available, :guests_id, :listings_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
