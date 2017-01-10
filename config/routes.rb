d20c9c2e3b2aabcd1ebc8be15b412ea1e145edac
Raghu Betina <raghu@firstdraft.com>
2017-01-10 20:42:40 +0000

    Generate hosts resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :hosts
+  # Routes for the Host resource:
+  # READ
+  get "/hosts", :controller => "hosts", :action => "index"
+  get "/hosts/:id", :controller => "hosts", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
